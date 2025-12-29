2c04034e0923d07c13730e3066d7ab3470ca21df
Nick Meinhold <nick@enspyr.co>
2025-12-29 21:06:11 +1100

    Standardize C++ formatting with 2-space indentation

--- watermarking-docker/detect.cpp
+++ watermarking-docker/detect.cpp
@@ -11,131 +11,127 @@
 #include "watermarking-functions/Utilities.hpp"
 #include "watermarking-functions/WatermarkDetection.hpp"
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
+  // check args have been passed in
+  // args are: unique id for db entry, file path for original image, file path
+  // for marked image
+  if (argc != 4) {
+    std::cout << "incorrect number of arguments" << std::endl;
+    return -1;
+  }
 
-    // check args have been passed in
-    // args are: unique id for db entry, file path for original image, file path for marked image
-    if(argc != 4) {
-        std::cout << "incorrect number of arguments" << std::endl;
-        return -1;
+  std::string uid = argv[argc - 3];  // the userid, used in the file path for saving results
+  std::string originalFilePath = argv[argc - 2];
+  std::string markedFilePath = argv[argc - 1];
+  std::string outputFilePath = "/tmp/" + uid + ".json";
+
+  std::cout << "user with id " << uid << ", detecting message in marked image at " << markedFilePath
+            << std::endl;
+
+  int p, k, maxX, maxY, imgRows, imgCols;
+  double ms, peak2rms, maxVal;
+  std::vector<int> shifts;
+  std::vector<double> psnrs;
+
+  // read in images and convert to 3 channel BGR
+
+  cv::Mat original = cv::imread(originalFilePath, cv::IMREAD_COLOR);
+  cv::Mat marked = cv::imread(markedFilePath, cv::IMREAD_COLOR);
+
+  std::cout << "images read in and converted to 3 channel BGR " << std::endl;
+
+  // check original and marked images are of equal size
+  if (original.rows != marked.rows || original.cols != marked.cols) {
+    std::cout << "original and marked images are not equal sizes" << std::endl;
+    return -2;
+  }
+  // set variables for the image rows and cols, now we know original and marked
+  // are the same size
+  imgRows = original.rows;
+  imgCols = original.cols;
+
+  // calculate the largest prime for this image
+  p = largestPrimeFor(original);
+
+  std::cout << "largest prime was found to be " << p << std::endl;
+
+  // convert images to HSV
+
+  cv::Mat hsvOriginal, hsvMarked;
+  cvtColor(original, hsvOriginal, cv::COLOR_BGR2HSV);
+  cvtColor(marked, hsvMarked, cv::COLOR_BGR2HSV);
+
+  // create 1d array for luma values
+  double* lumaArray = new double[imgCols * imgRows];
+  // subtract original luma from marked luma and store the result
+  for (int y = 0; y < imgRows; y++) {
+    for (int x = 0; x < imgCols; x++) {
+      lumaArray[y * imgCols + x] =
+          (hsvMarked.at<cv::Vec3b>(y, x).val[2] - hsvOriginal.at<cv::Vec3b>(y, x).val[2]) / 255.0;
     }
-    
-    std::string uid = argv[argc-3]; // the userid, used in the file path for saving results 
-    std::string originalFilePath = argv[argc-2];
-    std::string markedFilePath = argv[argc-1];
-    std::string outputFilePath = "/tmp/"+uid+".json";
+  }
 
-    std::cout << "user with id " << uid << ", detecting message in marked image at " << markedFilePath <<  std::endl;
+  // extract the watermark from the frequency domain
+  double* extractedMark = new double[p * p];
+  extractMark(hsvOriginal.rows, hsvOriginal.cols, p, p, lumaArray, extractedMark);
 
-    int p, k, maxX, maxY, imgRows, imgCols;
-    double ms, peak2rms, maxVal; 
-    std::vector<int> shifts; 
-    std::vector<double> psnrs; 
-    
-    // read in images and convert to 3 channel BGR
+  double* wmArray = new double[p * p];
+  double* correlationVals = new double[p * p];
 
-    cv::Mat original = cv::imread(originalFilePath, cv::IMREAD_COLOR);
-    cv::Mat marked = cv::imread(markedFilePath, cv::IMREAD_COLOR);
+  // perform detection for each family of arrays (family determined by k value)
 
-    std::cout << "images read in and converted to 3 channel BGR " << std::endl;
+  k = 1;
+  while (1) {
+    // generate each array and perform correlation
+    generateArray(p, k, wmArray);
+    fastCorrelation(p, p, extractedMark, wmArray, correlationVals);
 
-    // check original and marked images are of equal size 
-    if(original.rows != marked.rows || original.cols != marked.cols) {
-        std::cout << "original and marked images are not equal sizes" << std::endl;
-        return -2;
-    }
-    // set variables for the image rows and cols, now we know original and marked are the same size 
-    imgRows = original.rows;
-    imgCols = original.cols;
-    
-    // calculate the largest prime for this image
-    p = largestPrimeFor(original);
-    
-    std::cout << "largest prime was found to be " << p << std::endl;
-    
-    // convert images to HSV
-    
-    cv::Mat hsvOriginal, hsvMarked;
-    cvtColor(original, hsvOriginal, cv::COLOR_BGR2HSV);
-    cvtColor(marked, hsvMarked, cv::COLOR_BGR2HSV);
-    
-    // create 1d array for luma values 
-    double* lumaArray = new double[imgCols*imgRows];
-    // subtract original luma from marked luma and store the result 
-    for(int y = 0; y < imgRows; y++)
-    {
-        for(int x = 0; x < imgCols; x++)
-        {
-            lumaArray[y*imgCols + x] = (hsvMarked.at<cv::Vec3b>(y,x).val[2] - hsvOriginal.at<cv::Vec3b>(y,x).val[2]) / 255.0;
+    // calculate peak value and peak2rms for this family of arrays
+    maxVal = 0.0;
+    maxX = -1;
+    maxY = -1;
+    for (int y = 0; y < p; y++) {
+      for (int x = 0; x < p; x++) {
+        if (correlationVals[y * p + x] > maxVal) {
+          maxVal = correlationVals[y * p + x];
+          maxY = y;
+          maxX = x;
         }
+      }
     }
 
-    // extract the watermark from the frequency domain 
-    double *extractedMark = new double[p*p];
-    extractMark(hsvOriginal.rows, hsvOriginal.cols, p, p, lumaArray, extractedMark);
+    // calculate peak2rms
+    ms = 0;
+    for (int i = 0; i < p * p; i++)
+      ms += (correlationVals[i] * correlationVals[i]) / (p * p);
+    peak2rms = maxVal / sqrt(ms);
 
-    double* wmArray = new double[p*p];
-    double* correlationVals = new double[p*p]; 
-    
-    // perform detection for each family of arrays (family determined by k value) 
-        
-    k = 1;
-    while(1) {
+    // increment k to move on to next family
+    k++;
 
-        // generate each array and perform correlation 
-        generateArray(p, k, wmArray);
-        fastCorrelation(p, p, extractedMark, wmArray, correlationVals);
+    if (peak2rms > 6) {
+      shifts.push_back(maxY * p +
+                       maxX);  // store the detected shift (the shift that had the peak value)
+      psnrs.push_back(peak2rms);
 
-        // calculate peak value and peak2rms for this family of arrays 
-        maxVal = 0.0; maxX = -1; maxY = -1;
-        for(int y = 0; y < p; y++)
-        {
-            for(int x = 0; x < p; x++)
-            {
-                if(correlationVals[y*p+x] > maxVal)
-                {
-                    maxVal = correlationVals[y*p+x];
-                    maxY = y;
-                    maxX = x;
-                }
-            }
-        }
-        
-        // calculate peak2rms 
-        ms = 0;
-        for (int i = 0; i < p*p; i++) ms += (correlationVals[i] * correlationVals[i]) / (p*p);
-        peak2rms = maxVal / sqrt(ms);
-        
-        // increment k to move on to next family 
-        k++;
-        
-        if(peak2rms > 6) {
+    } else {
+      // TODO: if keeping the last peak, it will need to be removed before being
+      // used in getASCII
 
-            shifts.push_back(maxY*p+maxX); // store the detected shift (the shift that had the peak value) 
-            psnrs.push_back(peak2rms); 
+      // add the last peak as well, so that results include the last rejected
+      // peak shifts.push_back(maxY*p+maxX); psnrs.push_back(peak2rms);
 
-        }
-        else {
-
-            // TODO: if keeping the last peak, it will need to be removed before being used in getASCII 
-
-            // add the last peak as well, so that results include the last rejected peak  
-            // shifts.push_back(maxY*p+maxX); 
-            // psnrs.push_back(peak2rms); 
-
-            break;
-        }
-
+      break;
     }
+  }
 
-    std::string messageStr = "No message found.";
-    // calculate the message from the shifts 
-    if(shifts.size() != 0) {
-        messageStr = getASCII(shifts, p*p); 
-    }
+  std::string messageStr = "No message found.";
+  // calculate the message from the shifts
+  if (shifts.size() != 0) {
+    messageStr = getASCII(shifts, p * p);
+  }
 
-    outputResultsFile(messageStr, outputFilePath);
+  outputResultsFile(messageStr, outputFilePath);
 
-    return 0;
-
+  return 0;
 }
--- watermarking-docker/mark.cpp
+++ watermarking-docker/mark.cpp
@@ -7,125 +7,120 @@
 //
 
 #include <iostream>
-
 #include <opencv2/opencv.hpp>
 
 #include "watermarking-functions/Utilities.hpp"
 #include "watermarking-functions/WatermarkDetection.hpp"
 
-int main(int argc, const char * argv[]) {
-    
-    // check args have been passed in
-    // args are: file path, image name, message, strength
-    if(argc != 5) {
-        std::cout << "incorrect number of arguments" << std::endl;
-        return -1;
+int main(int argc, const char* argv[]) {
+  // check args have been passed in
+  // args are: file path, image name, message, strength
+  if (argc != 5) {
+    std::cout << "incorrect number of arguments" << std::endl;
+    return -1;
+  }
+
+  // std::cout << "image name: " << argv[argc-3] << ", message = " <<
+  // argv[argc-2] << std::endl;
+
+  int strength = atoi(argv[argc - 1]);
+  std::string strengthString = argv[argc - 1];
+  std::string message = argv[argc - 2];
+  std::string imageName = argv[argc - 3];
+  std::string filePath = argv[argc - 4];
+
+  // read in image and convert to 3 channel BGR
+
+  cv::Mat original = cv::imread(filePath, cv::IMREAD_COLOR);
+
+  std::cout << "PROGRESS:loading" << std::endl;
+  std::cout.flush();
+
+  // calculate the largest prime for this image
+
+  int p = largestPrimeFor(original);
+
+  // convert image to HSV
+
+  cv::Mat hsvImage;
+  cvtColor(original, hsvImage, cv::COLOR_BGR2HSV);
+
+  // create a 1d array with luma values
+
+  double* lumaArray = new double[hsvImage.cols * hsvImage.rows];
+
+  for (int y = 0; y < hsvImage.rows; y++) {
+    for (int x = 0; x < hsvImage.cols; x++) {
+      lumaArray[y * hsvImage.cols + x] = hsvImage.at<cv::Vec3b>(y, x).val[2] / 255.0;
     }
-    
-    // std::cout << "image name: " << argv[argc-3] << ", message = " << argv[argc-2] << std::endl;
-    
-    int strength = atoi(argv[argc-1]);
-    std::string strengthString = argv[argc-1];
-    std::string message = argv[argc-2];
-    std::string imageName = argv[argc-3];
-    std::string filePath = argv[argc-4];
-    
-    // read in image and convert to 3 channel BGR
+  }
 
-    cv::Mat original = cv::imread(filePath, cv::IMREAD_COLOR);
+  // create the watermark array
 
-    std::cout << "PROGRESS:loading" << std::endl;
+  double* wmArray = new double[p * p];
+
+  // generate each array and mark the image
+
+  std::vector<int> messageShifts = getShifts(message, p * p);
+  int totalShifts = (int)messageShifts.size();
+
+  for (int k = 1; k <= totalShifts; k++) {
+    std::cout << "PROGRESS:marking:" << k << ":" << totalShifts << std::endl;
     std::cout.flush();
 
-    // calculate the largest prime for this image
-    
-    int p = largestPrimeFor(original);
-    
-    // convert image to HSV
-    
-    cv::Mat hsvImage;
-    cvtColor(original, hsvImage, cv::COLOR_BGR2HSV);
-    
-    // create a 1d array with luma values
-    
-    double* lumaArray = new double[hsvImage.cols*hsvImage.rows];
-    
-    for(int y = 0; y < hsvImage.rows; y++)
-    {
-        for(int x = 0; x < hsvImage.cols; x++)
-        {
-            lumaArray[y*hsvImage.cols + x] = hsvImage.at<cv::Vec3b>(y,x).val[2] / 255.0;
-        }
-    }
-    
-    // create the watermark array
-    
-    double* wmArray = new double[p*p];
-    
-    // generate each array and mark the image
-    
-    std::vector<int> messageShifts = getShifts(message, p*p);
-    int totalShifts = (int)messageShifts.size();
+    generateArray(p, k, wmArray);
 
-    for(int k = 1; k <= totalShifts; k++) {
+    // multiply the watermark array by the strength
 
-        std::cout << "PROGRESS:marking:" << k << ":" << totalShifts << std::endl;
-        std::cout.flush();
+    for (int i = 0; i < p * p; i++) {
+      wmArray[i] = wmArray[i] * strength;
+    }
 
-        generateArray(p, k, wmArray);
-        
-        // multiply the watermark array by the strength
-        
-        for(int i = 0; i < p*p; i++) {
-            wmArray[i] = wmArray[i]*strength;
-        }
-        
-        // mark the luma data
-        
-        insertMark(hsvImage.rows, hsvImage.cols, p, p, lumaArray, wmArray, messageShifts[k-1]);
-        
-    }
-    
-    // put the marked luma data back into the original image
-    
-    for(int y = 0; y < hsvImage.rows; y++)
-    {
-        for(int x = 0; x < hsvImage.cols; x++)
-        {
-            float lumaValue = lumaArray[y*hsvImage.cols + x] * 255.0;
-            
-            if(lumaValue > 255.0) hsvImage.at<cv::Vec3b>(y,x).val[2] = 255;
-            else if (lumaValue < 0.0) hsvImage.at<cv::Vec3b>(y,x).val[2] = 0;
-            else {
-                hsvImage.at<cv::Vec3b>(y,x).val[2] = (int)(round(lumaValue));
-            }
-            
-        }
-    }
-    
-    // convert back to BGR (required by imwrite)
-    
-    cvtColor(hsvImage, original, cv::COLOR_HSV2BGR);
-    
-    // IMWRITE_PNG_COMPRESSION
-    // compression level from 0 to 9. A higher value means a smaller size and longer compression time
+    // mark the luma data
 
-    std::cout << "PROGRESS:saving" << std::endl;
-    std::cout.flush();
+    insertMark(hsvImage.rows, hsvImage.cols, p, p, lumaArray, wmArray, messageShifts[k - 1]);
+  }
 
-    std::vector<int> compression_params;
-    compression_params.push_back(cv::IMWRITE_PNG_COMPRESSION);
-    compression_params.push_back(9);
-    
-    try {
-        imwrite(filePath + "-marked.png", original, compression_params);
+  // put the marked luma data back into the original image
+
+  for (int y = 0; y < hsvImage.rows; y++) {
+    for (int x = 0; x < hsvImage.cols; x++) {
+      float lumaValue = lumaArray[y * hsvImage.cols + x] * 255.0;
+
+      if (lumaValue > 255.0)
+        hsvImage.at<cv::Vec3b>(y, x).val[2] = 255;
+      else if (lumaValue < 0.0)
+        hsvImage.at<cv::Vec3b>(y, x).val[2] = 0;
+      else {
+        hsvImage.at<cv::Vec3b>(y, x).val[2] = (int)(round(lumaValue));
+      }
     }
-    catch (cv::Exception& ex) {
-        fprintf(stderr, "Exception writing out image to PNG format: %s\n", ex.what());
-        return 1;
-    }
-    
-    // std::cout << "file is " << hsvImage.rows << " x " << hsvImage.cols << " image of type " << ocv_type2str(hsvImage.type()) << std::endl;
-    
-    return 0;
+  }
+
+  // convert back to BGR (required by imwrite)
+
+  cvtColor(hsvImage, original, cv::COLOR_HSV2BGR);
+
+  // IMWRITE_PNG_COMPRESSION
+  // compression level from 0 to 9. A higher value means a smaller size and
+  // longer compression time
+
+  std::cout << "PROGRESS:saving" << std::endl;
+  std::cout.flush();
+
+  std::vector<int> compression_params;
+  compression_params.push_back(cv::IMWRITE_PNG_COMPRESSION);
+  compression_params.push_back(9);
+
+  try {
+    imwrite(filePath + "-marked.png", original, compression_params);
+  } catch (cv::Exception& ex) {
+    fprintf(stderr, "Exception writing out image to PNG format: %s\n", ex.what());
+    return 1;
+  }
+
+  // std::cout << "file is " << hsvImage.rows << " x " << hsvImage.cols << "
+  // image of type " << ocv_type2str(hsvImage.type()) << std::endl;
+
+  return 0;
 }
